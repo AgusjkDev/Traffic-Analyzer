d2cfff3f923ab49b1dbe5f526d9423fb4df6dd24
AgusjkDev <agusjkdev7@gmail.com>
2023-05-17 14:36:08 -0300

    change buttons size

--- packages/website/components/SigninWithProviderButtons.tsx
+++ packages/website/components/SigninWithProviderButtons.tsx
@@ -14,16 +14,16 @@ export default function SigninWithProviderButtons() {
     };
 
     return (
-        <div className="flex w-full flex-col items-center gap-y-3 md:gap-y-5 2xl:gap-y-6">
+        <div className="flex flex-col items-center w-full gap-y-3 md:gap-y-5 2xl:gap-y-6">
             {signinWithProviderButtons.map(({ provider, displayName, svg }) => (
                 <button
                     key={provider}
                     onClick={() => handleSignin(provider)}
-                    className="group grid w-full max-w-[280px] grid-cols-4 place-items-center rounded-sm border-[1px] border-primary bg-white py-2.5 transition-colors duration-300 hover:border-emerald-600 lg:max-w-xs lg:py-3"
+                    className="group grid w-full max-w-[280px] grid-cols-4 place-items-center rounded-sm border-[1px] border-primary bg-white py-2.5 transition-colors duration-300 hover:border-emerald-600 lg:max-w-xs"
                 >
                     <Svg {...svg} width={24} height={24} />
 
-                    <span className="col-span-3 w-full text-left text-sm transition-colors duration-300 group-hover:text-emerald-600 lg:text-base">
+                    <span className="w-full col-span-3 text-sm text-left transition-colors duration-300 group-hover:text-emerald-600 lg:text-base">
                         Iniciar sesión con {displayName}
                     </span>
                 </button>
