24d3071b8e338b11e6ad1ff6e5c618621230268b
AgusjkDev <agusjkdev7@gmail.com>
2023-05-19 13:49:06 -0300

    fix transparent dashboard menu

--- packages/website/components/DashboardMenu.tsx
+++ packages/website/components/DashboardMenu.tsx
@@ -5,7 +5,7 @@ import { dashboardAnchors } from "data";
 
 export default function DashboardMenu() {
     return (
-        <aside className="fixed bottom-0 flex w-full justify-evenly border-t-[1px] border-gray-100 p-2.5 md:relative md:bottom-auto md:w-auto md:flex-col md:justify-start md:gap-y-3.5 md:border-r-[1px] md:border-t-0 md:p-3.5 md:[&>*:last-child]:mt-auto">
+        <aside className="fixed bottom-0 flex w-full justify-evenly border-t-[1px] border-gray-100 bg-white p-2.5 md:relative md:bottom-auto md:w-auto md:flex-col md:justify-start md:gap-y-3.5 md:border-r-[1px] md:border-t-0 md:p-3.5 md:[&>*:last-child]:mt-auto">
             {dashboardAnchors.map(({ href, label, title, svg }) => (
                 <Link
                     key={label}
