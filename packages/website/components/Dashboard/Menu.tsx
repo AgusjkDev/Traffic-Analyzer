847b664daf6529e24971b204cd315106636e651f
AgusjkDev <agusjkdev7@gmail.com>
2023-05-19 23:13:02 -0300

    add active icon state

--- packages/website/components/Dashboard/Menu.tsx
+++ packages/website/components/Dashboard/Menu.tsx
@@ -1,22 +1,32 @@
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 import { Svg } from "components";
 import { dashboardAnchors } from "data";
 
 export default function DashboardMenu() {
+    const pathname = usePathname();
+
     return (
         <aside className="fixed bottom-0 flex w-full justify-evenly border-t-[1px] border-gray-100 bg-white p-2.5 md:relative md:bottom-auto md:w-auto md:flex-col md:justify-start md:gap-y-3.5 md:border-r-[1px] md:border-t-0 md:p-3.5 md:[&>*:last-child]:mt-auto">
-            {dashboardAnchors.map(({ href, label, title, svg }) => (
-                <Link
-                    key={label}
-                    aria-label={label}
-                    title={title}
-                    href={href}
-                    className="group aspect-square w-6"
-                >
-                    <Svg {...svg} width={24} height={24} />
-                </Link>
-            ))}
+            {dashboardAnchors.map(({ href, label, title, svg }) => {
+                return (
+                    <Link
+                        key={label}
+                        aria-label={label}
+                        title={title}
+                        href={href}
+                        className="group aspect-square w-6"
+                    >
+                        <Svg
+                            {...svg}
+                            className={pathname === href ? "fill-secondary" : undefined}
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+                );
+            })}
         </aside>
     );
 }
--- packages/website/components/Svg.tsx
+++ packages/website/components/Svg.tsx
@@ -1,18 +1,21 @@
 import type { SVG } from "data";
 
 interface SvgProps extends SVG {
+    className?: string;
     width?: number;
     height?: number;
 }
 
-export default function Svg({ d, width = 36, height = 36 }: SvgProps) {
+export default function Svg({ d, className, width = 36, height = 36 }: SvgProps) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width={width}
             height={height}
             viewBox="0 0 24 24"
-            className="fill-primary transition-colors duration-300 group-hover:fill-secondary"
+            className={`fill-primary transition-colors duration-300 group-hover:fill-secondary${
+                className ? ` ${className}` : ""
+            }`}
         >
             <path d={d} />
         </svg>
