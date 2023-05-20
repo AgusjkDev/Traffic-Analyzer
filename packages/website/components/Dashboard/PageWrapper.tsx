9de1c6a48184959f09493c03f7c7621c56d6165a
AgusjkDev <agusjkdev7@gmail.com>
2023-05-20 15:07:04 -0300

    change dashboard page wrapper

--- packages/website/components/Dashboard/PageWrapper.tsx
+++ packages/website/components/Dashboard/PageWrapper.tsx
@@ -1,3 +1,5 @@
+import { Divider } from "@tremor/react";
+
 interface PageWrapperProps {
     title: React.ReactNode;
     introduction: React.ReactNode;
@@ -6,13 +8,15 @@ interface PageWrapperProps {
 
 export default function PageWrapper({ title, introduction, children }: PageWrapperProps) {
     return (
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
             <div className="flex flex-col gap-3 md:gap-4">
                 <h2 className="text-lg text-primary md:text-2xl">{title}</h2>
                 <p className="max-w-prose text-sm text-primary-light md:max-w-prose-lg md:text-base">
                     {introduction}
                 </p>
             </div>
+
+            <Divider />
 
             {children}
         </div>
