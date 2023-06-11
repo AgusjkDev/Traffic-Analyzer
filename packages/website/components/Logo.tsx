71d8446a83a010bcf90b50115a2a4c3f3483d468
AgusjkDev <agusjkdev7@gmail.com>
2023-06-11 14:26:50 -0300

    add priority to logo image component

--- packages/website/components/Logo.tsx
+++ packages/website/components/Logo.tsx
@@ -4,7 +4,13 @@ import Image from "next/image";
 export default function Logo() {
     return (
         <Link aria-label="Ir al Inicio" title="Ir al Inicio" href="/">
-            <Image alt="Logo de Traffic Analyzer" src="/imgs/logo.png" width={470} height={140} />
+            <Image
+                alt="Logo de Traffic Analyzer"
+                src="/imgs/logo.png"
+                priority
+                width={470}
+                height={140}
+            />
         </Link>
     );
 }
