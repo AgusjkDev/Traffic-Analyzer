3f9138873b12bb730471b0326eefc31dbb955331
AgusjkDev <agusjkdev7@gmail.com>
2023-07-12 00:19:48 -0300

    remove unnecessary logic

--- packages/website/components/SigninWithProviderButtons.tsx
+++ packages/website/components/SigninWithProviderButtons.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useContext } from "react";
-import type { Provider } from "@supabase/supabase-js";
 
 import { SupabaseContext } from "context";
 import Svg from "./Svg";
@@ -9,16 +8,12 @@ import { signinWithProviderButtons } from "data";
 export default function SigninWithProviderButtons() {
     const { signinWithProvider } = useContext(SupabaseContext);
 
-    const handleSignin = (provider: Provider) => {
-        signinWithProvider(provider);
-    };
-
     return (
         <div className="flex w-full flex-col items-center gap-y-3 md:gap-y-5 2xl:gap-y-6">
             {signinWithProviderButtons.map(({ provider, displayName, svg }) => (
                 <button
                     key={provider}
-                    onClick={() => handleSignin(provider)}
+                    onClick={() => signinWithProvider(provider)}
                     className="group grid w-full max-w-[280px] grid-cols-4 place-items-center rounded-sm border-[1px] border-primary bg-white py-2.5 transition-colors duration-300 hover:border-secondary lg:max-w-xs"
                 >
                     <Svg {...svg} width={24} height={24} />
