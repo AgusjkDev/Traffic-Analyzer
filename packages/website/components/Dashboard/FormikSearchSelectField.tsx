438f8b55eea1879b73771fd7dfee3c6e2c93f513
AgusjkDev <agusjkdev7@gmail.com>
2023-07-11 23:55:59 -0300

    add useSearch hook

--- packages/website/components/Dashboard/FormikSearchSelectField.tsx
+++ packages/website/components/Dashboard/FormikSearchSelectField.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { useFormikContext, type FieldInputProps } from "formik";
 
-import { compareSearch } from "helpers";
+import { useSearch } from "hooks";
 import type { FormValues } from "data";
 
 interface FormikSearchSelectFieldProps extends FieldInputProps<string> {
@@ -14,9 +14,9 @@ const PROPS_TO_REPLACE = ["focusId", "options"];
 
 export default function FormikSearchSelectField(props: FormikSearchSelectFieldProps) {
     const [isFocused, setIsFocused] = useState(false);
-    const [options, setOptions] = useState(props.options);
     const inputRef = useRef<HTMLInputElement>(null);
     const formikContext = useFormikContext<FormValues>();
+    const matchings = useSearch(props.options, props.value);
 
     const handleSelectFocus = () => {
         inputRef.current?.focus();
@@ -42,10 +42,6 @@ export default function FormikSearchSelectField(props: FormikSearchSelectFieldPr
         {}
     );
 
-    useEffect(() => {
-        setOptions(props.options.filter(option => compareSearch(option, props.value))); // TODO: To improve
-    }, [props.value]);
-
     return (
         <div className="relative w-full">
             <select id={props.focusId} className="h-0 w-0 opacity-0" onFocus={handleSelectFocus} />
@@ -60,9 +56,9 @@ export default function FormikSearchSelectField(props: FormikSearchSelectFieldPr
                 className="w-full rounded-md border-[1px] border-gray-300 bg-white p-2.5 text-sm transition-colors duration-300 placeholder:text-primary-light focus:border-gray-400 focus:outline-none"
             />
 
-            {isFocused && options.length > 0 && (
+            {isFocused && matchings.length > 0 && (
                 <div className="absolute top-11 z-[1] flex max-h-[228px] w-full flex-col divide-y overflow-y-auto rounded-md border-[1px] border-gray-300 bg-red-400 text-sm">
-                    {options.map(option => (
+                    {matchings.map(option => (
                         <button
                             key={option}
                             onMouseDown={() => handleOptionClick(option)}
---
+++ packages/website/hooks/useSearch.ts
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+import { compareSearch } from "helpers";
+
+export default function useSearch(terms: string[], term: string) {
+    const [matchings, setMatchings] = useState<string[]>([]);
+
+    useEffect(() => {
+        setMatchings(terms.filter(option => compareSearch(option, term))); // TODO: To improve
+    }, [term]);
+
+    return matchings;
+}
