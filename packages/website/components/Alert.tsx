08ac1fdfa7eb36073cc9ab21ed9cbbbd310506dd
AgusjkDev <agusjkdev7@gmail.com>
2023-07-13 16:14:39 -0300

    add final alert styles

--- packages/website/components/Alert.tsx
+++ packages/website/components/Alert.tsx
@@ -22,26 +22,27 @@ export default function Alert({ success, children, hideAlert }: AlertProps) {
         <button
             ref={alertRef}
             onClick={startHidingAnimation}
-            className="group relative grid grid-cols-6 place-items-center rounded-sm border-[1px] border-gray-300 bg-white p-2.5 transition duration-500 hover:border-gray-400"
+            className="group pointer-events-auto relative flex items-center gap-x-2.5 rounded-sm border-[1px] border-gray-300 bg-white p-2.5 transition duration-500 hover:border-gray-400"
         >
             <Svg
                 {...(success ? svgs.checkCircle : svgs.checkX)}
                 width={20}
                 height={20}
                 className={twMerge(
+                    "flex-shrink-0",
                     success
                         ? "fill-green-600 group-hover:fill-green-700"
                         : "fill-red-600 group-hover:fill-red-700"
                 )}
             />
 
-            <span className="col-span-5 text-xs text-primary-light transition-colors duration-300 [text-wrap:balance] group-hover:text-primary">
+            <span className="max-w-[225px] text-xs text-primary-light transition-colors duration-300 [text-wrap:balance] group-hover:text-primary">
                 {children}
             </span>
 
             <div
                 className={twMerge(
-                    "absolute bottom-0 -mt-[2px] h-[2px] w-full animate-timer rounded-b-sm transition-colors duration-300",
+                    "absolute bottom-0 left-0 -mt-[2px] h-[2px] w-full animate-timer rounded-b-sm transition-colors duration-300",
                     success
                         ? "bg-green-600 group-hover:bg-green-700"
                         : "bg-red-600 group-hover:bg-red-700"
--- packages/website/context/AlertsContext/AlertsProvider.tsx
+++ packages/website/context/AlertsContext/AlertsProvider.tsx
@@ -19,7 +19,7 @@ export default function AlertsProvider({ children }: PropsWithChildren) {
 
     return (
         <AlertsContext.Provider value={{ addAlert }}>
-            <div className="fixed bottom-2 right-2 z-[2] flex max-w-[275px] flex-col-reverse items-end gap-y-3 md:bottom-auto md:top-[104.56px] md:flex-col">
+            <div className="pointer-events-none fixed bottom-[calc(45px+8px)] right-2 z-[2] flex flex-col-reverse items-end gap-y-3 md:bottom-auto md:top-[calc(81px+8px)] md:flex-col">
                 {alerts.map(({ id, success, message }) => (
                     <Alert key={id} success={success} hideAlert={() => hideAlert(id)}>
                         {message}
