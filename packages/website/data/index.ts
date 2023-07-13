e901a5b862f011c387ee91df30e0bd0dd23406f4
AgusjkDev <agusjkdev7@gmail.com>
2023-07-13 01:15:28 -0300

    fix barrels import orders

--- packages/website/data/index.ts
+++ packages/website/data/index.ts
@@ -1,10 +1,10 @@
+export * as env from "./env";
 export { default as navBarAnchors } from "./navBarAnchors";
 export * as svgs from "./svgs";
-export { default as signinWithProviderButtons } from "./signinWithProviderButtons";
 export type { SVG } from "./svgs";
+export { default as signinWithProviderButtons } from "./signinWithProviderButtons";
 export { default as dashboardAnchors } from "./dashboardAnchors";
 export { default as dashboardCards } from "./dashboardCards";
-export * as env from "./env";
 export { default as devicesActionTabs } from "./devicesActionTabs";
 export {
     default as devicesActivateForm,
