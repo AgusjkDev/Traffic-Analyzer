facdaccf492a8a2e3df5520cbfa9fa51d4ca1d73
AgusjkDev <agusjkdev7@gmail.com>
2023-07-13 23:14:34 -0300

    update dashboard index page title

--- packages/website/app/dashboard/page.tsx
+++ packages/website/app/dashboard/page.tsx
@@ -1,8 +1,18 @@
-import { PageWrapper, WelcomeTitle, WelcomeText, SystemStatus, Cards } from "components/Dashboard";
+"use client";
+import { useContext } from "react";
+
+import { SupabaseContext } from "context";
+import { PageWrapper, WelcomeText, SystemStatus, Cards } from "components/Dashboard";
 
 export default function Dashboard() {
+    const { session } = useContext(SupabaseContext);
+
+    const fullname: string | undefined = session?.user.user_metadata.full_name;
+
+    const title = `¡Hola de nuevo${fullname ? `, ${fullname}` : ""}!`;
+
     return (
-        <PageWrapper title={<WelcomeTitle />} introduction={<WelcomeText />}>
+        <PageWrapper title={title} introduction={<WelcomeText />}>
             <div className="flex flex-col gap-y-8">
                 <SystemStatus />
 
--- packages/website/components/Dashboard/index.ts
+++ packages/website/components/Dashboard/index.ts
@@ -1,6 +1,5 @@
 export { default as Layout } from "./Layout";
 export { default as PageWrapper } from "./PageWrapper";
-export { default as WelcomeTitle } from "./WelcomeTitle";
 export { default as WelcomeText } from "./WelcomeText";
 export { default as SystemStatus } from "./SystemStatus";
 export { default as Cards } from "./Cards";
